9276b809372ca28c22ef98fb27abd212f6ddd2c9
jdh930402 <partialite@naver.com>
2019-07-24 16:17:26 +0900

    2019-07-24

--- src/java_20190723/CalenadarDemo.java
+++ src/java_20190723/CalenadarDemo.java
@@ -2,6 +2,7 @@ package java_20190723;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class CalenadarDemo {
 
--- src/java_20190723/Calendar.java
+++ src/java_20190723/Calendar.java
@@ -1,57 +1,61 @@
 package java_20190723;
 
 public class Calendar {
+	private String[] date = { "일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일" };
+	private int totalCount = 0;
+	private int dayOfWeek = 0;
 
-	
-	private boolean isLeafYear(int year) {
-		return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-	}
-	
+	//윤년을 판단하고 한달에 몇일인지 판단하는 메서드
 	private int getLastDay(int year, int month) {
 		int[] monthArray = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		if (isLeafYear(year)) {
+		boolean isLeafYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+		if (isLeafYear) {
 			monthArray[1] = 29;
 		}
 		return monthArray[month - 1];
 	}
 
+	//총 일수를 구하는 메서드
 	private int getCount(int year, int month, int day) {
 		int preYear = year - 1;
 		int preMonth = month - 1;
-		int totalCount = 0;
 		getLastDay(year, month);
 		totalCount = preYear * 365 + (preYear / 4 - preYear / 100 + preYear / 400);
 
 		// 2019년도 11월까지 합 구하기
 		for (int i = 0; i < preMonth; i++) {
-			totalCount += getLastDay(year,i+1);
+			totalCount += getLastDay(year, i + 1);
 		}
 
 		totalCount += day;
 		return totalCount;
 	}
 
+	//요일을 판단하는 메서드
+	public int dayOfWeek(int totalCount) {
+		return totalCount % 7;
+	}
+	
 	public void print(int year, int month, int day) {
-		String[] date = {"일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"}; 
-		int totalCount = getCount(year, month, day);
-		int dayOfWeek = totalCount % 7;
+		totalCount = getCount(year, month, day);
+		dayOfWeek = dayOfWeek(totalCount);
 		System.out.printf("%d년 %d월 %d일은 %s입니다.\n", year, month, day, date[dayOfWeek]);
 	}
 
+
 	public void print(int year, int month) {
-		int totalCount = getCount(year,month,1);
-		int dayOfWeek = totalCount % 7;
-		String[] date = {"일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"}; 
-		for(int i=0 ; i<date.length ; i++) {
-			System.out.print(date[i]+"\t");
+		totalCount = getCount(year, month, 1);
+		dayOfWeek = dayOfWeek(totalCount);
+		for (int i = 0; i < date.length; i++) {
+			System.out.print(date[i] + "\t");
 		}
 		System.out.println();
-		for(int i=0 ; i < dayOfWeek ; i++) {
+		for (int i = 0; i < dayOfWeek; i++) {
 			System.out.print("\t");
 		}
-		for(int i=0 ; i< getLastDay(year,month); i++) {
-			System.out.print( (i+1)+ "\t");
-			if(dayOfWeek%7 == 6) {
+		for (int i = 0; i < getLastDay(year, month); i++) {
+			System.out.print((i + 1) + "\t");
+			if (dayOfWeek % 7 == 6) {
 				System.out.println();
 			}
 			dayOfWeek++;
@@ -60,8 +64,8 @@ public class Calendar {
 	}
 
 	public void print(int year) {
-		for(int i=0 ; i<12; i++) {
-		print(year,i+1);
+		for (int i = 0; i < 12; i++) {
+			print(year, i + 1);
 		}
 		System.out.println();
 	}
